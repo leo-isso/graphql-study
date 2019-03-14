db2692fb4338d4cb1462df8529e54de69162b81b
Leonardo Isso <leoisso.work@gmail.com>
2019-03-14 00:19:40 -0300

    Add serve to script

---
+++ dist/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _expressGraphql = require('express-graphql');
+
+var _expressGraphql2 = _interopRequireDefault(_expressGraphql);
+
+var _index = require('./index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.use('graphql/', (0, _expressGraphql2.default)({
+  schema: _index2.default,
+  graphiql: true
+}));
+
+console.log('Starting the server!');
+app.listen(5050);
